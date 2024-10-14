239bf093205646eee3ad2e5c3b86a723eddb0625
mhndckngyn <126580562+mhndckngyn@users.noreply.github.com>
2024-10-14 15:28:36 +0700

    Connect Database

---
+++ Migrations/TLoaiSp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace se310_th2.Migrations;
+
+public partial class TLoaiSp
+{
+    public string MaLoai { get; set; } = null!;
+
+    public string? Loai { get; set; }
+
+    public virtual ICollection<TDanhMucSp> TDanhMucSps { get; set; } = new List<TDanhMucSp>();
+}

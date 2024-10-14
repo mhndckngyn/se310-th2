b9af97aced1484492e46cc93037a10e78a8d2521
mhndckngyn <126580562+mhndckngyn@users.noreply.github.com>
2024-10-14 18:40:53 +0700

    Add products to Index and Pagination

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,30 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using se310_th2.Context;
+using se310_th2.Migrations;
 using se310_th2.Models;
+using X.PagedList;
 
 namespace se310_th2.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
+    private MyDbContext db = new MyDbContext();
+    
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(int? page)
     {
-        return View();
+        const int PAGE_SIZE = 12;
+        int pageNumber = (int)(page is null or < 0 ? 1 : page);
+        var products = db.TDanhMucSps.AsNoTracking().OrderBy(x => x.TenSp);
+        PagedList<TDanhMucSp> pagedProducts = new PagedList<TDanhMucSp>(products, pageNumber, PAGE_SIZE);
+        return View(pagedProducts);
     }
 
     public IActionResult Privacy()

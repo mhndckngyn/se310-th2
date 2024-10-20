58476a5d0f6dba7c9cda885d8f39f103fd168d92
mhndckngyn <126580562+mhndckngyn@users.noreply.github.com>
2024-10-20 18:12:28 +0700

    B10: Add create product page for admin

--- Areas/Admin/Controllers/AdminHomeController.cs
+++ Areas/Admin/Controllers/AdminHomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using se310_th2.Context;
 using se310_th2.Models;
@@ -29,4 +30,30 @@ public class AdminHomeController : Controller
         PagedList<TDanhMucSp> pagedProducts = new PagedList<TDanhMucSp>(products, pageNumber, PAGE_SIZE);
         return View(pagedProducts);
     }
+
+    [Route("themsanphammoi")]
+    [HttpGet]
+    public IActionResult ThemSanPhamMoi()
+    {
+        ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
+        ViewBag.MaHangSx = new SelectList(db.THangSxes.ToList(), "MaHangSx", "HangSx");
+        ViewBag.MaNuocSx = new SelectList(db.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
+        ViewBag.MaLoai = new SelectList(db.TLoaiSps.ToList(), "MaLoai", "Loai");
+        ViewBag.MaDt = new SelectList(db.TLoaiDts.ToList(), "MaDt", "TenLoai");
+        return View();
+    }
+
+    [Route("themsanphammoi")]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult ThemSanPhamMoi(TDanhMucSp sanPham)
+    {
+        if (ModelState.IsValid)
+        {
+            db.TDanhMucSps.Add(sanPham);
+            db.SaveChanges();
+            return RedirectToAction("DanhMucSanPham");
+        }
+        return View(sanPham);
+    } 
 }

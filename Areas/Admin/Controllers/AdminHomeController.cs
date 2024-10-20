5140655c20f6b28b210f886f9b3eea8b1ea49785
mhndckngyn <126580562+mhndckngyn@users.noreply.github.com>
2024-10-20 16:20:31 +0700

    B9: Add product list for admin

--- Areas/Admin/Controllers/AdminHomeController.cs
+++ Areas/Admin/Controllers/AdminHomeController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using se310_th2.Context;
+using se310_th2.Models;
+using X.PagedList;
 
 namespace se310_th2.Areas.Admin.Controllers;
 
@@ -7,11 +11,22 @@ namespace se310_th2.Areas.Admin.Controllers;
 [Route("admin/homeadmin")]
 public class AdminHomeController : Controller
 {
+    private MyDbContext db = new MyDbContext(); 
     // GET
     [Route("")]
     [Route("index")]
     public IActionResult Index()
     {
         return View();
+    }
+    
+    [Route("danhmucsanpham")]
+    public IActionResult DanhMucSanPham(int? page)
+    {
+        const int PAGE_SIZE = 16;
+        int pageNumber = page is null or < 0 ? 1 : page.Value;
+        var products = db.TDanhMucSps.AsNoTracking().OrderBy(x => x.TenSp);
+        PagedList<TDanhMucSp> pagedProducts = new PagedList<TDanhMucSp>(products, pageNumber, PAGE_SIZE);
+        return View(pagedProducts);
     }
 }
